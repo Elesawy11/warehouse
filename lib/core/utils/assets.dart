1d7c72f7039d32eb1d55f182b0c51d8e67f32d2f
AhmedElesawy <elesawy928@gmail.com>
2025-02-01 11:49:58 +0200

    helpers file

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  static const String imagesOnboarding1 = "assets/images/onboarding1.webp";
+
+  static const String imagesSplash = "assets/images/splash.png";
+
+  static const String imagesSplash12 = "assets/images/splash12.png";
+
+  static const String imagesTest = "assets/images/test.png";
+}
+
---
+++ lib/core/utils/color_manager.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+abstract class ColorManager {
+  
+  static const Color mainBlue = Color(0xFF3F72E9);
+}
