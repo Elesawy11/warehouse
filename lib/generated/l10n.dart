52f4833c82d90e6d05ee56badfa261047235c88c
AhmedElesawy <elesawy928@gmail.com>
2025-02-02 02:09:44 +0200

    add localization for app

---
+++ lib/generated/l10n.dart
@@ -0,0 +1,144 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'intl/messages_all.dart';
+
+// **************************************************************************
+// Generator: Flutter Intl IDE plugin
+// Made by Localizely
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, lines_longer_than_80_chars
+// ignore_for_file: join_return_with_assignment, prefer_final_in_for_each
+// ignore_for_file: avoid_redundant_argument_values, avoid_escaping_inner_quotes
+
+class S {
+  S();
+
+  static S? _current;
+
+  static S get current {
+    assert(
+      _current != null,
+      'No instance of S was loaded. Try to initialize the S delegate before accessing S.current.',
+    );
+    return _current!;
+  }
+
+  static const AppLocalizationDelegate delegate = AppLocalizationDelegate();
+
+  static Future<S> load(Locale locale) {
+    final name =
+        (locale.countryCode?.isEmpty ?? false)
+            ? locale.languageCode
+            : locale.toString();
+    final localeName = Intl.canonicalizedLocale(name);
+    return initializeMessages(localeName).then((_) {
+      Intl.defaultLocale = localeName;
+      final instance = S();
+      S._current = instance;
+
+      return instance;
+    });
+  }
+
+  static S of(BuildContext context) {
+    final instance = S.maybeOf(context);
+    assert(
+      instance != null,
+      'No instance of S present in the widget tree. Did you add S.delegate in localizationsDelegates?',
+    );
+    return instance!;
+  }
+
+  static S? maybeOf(BuildContext context) {
+    return Localizations.of<S>(context, S);
+  }
+
+  /// `Ease of Use`
+  String get onboarding1Title {
+    return Intl.message(
+      'Ease of Use',
+      name: 'onboarding1Title',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `An intuitive interface that requires no special skills allows users to quickly add, edit, and manage products effortlessly.`
+  String get onboarding1Subtitle {
+    return Intl.message(
+      'An intuitive interface that requires no special skills allows users to quickly add, edit, and manage products effortlessly.',
+      name: 'onboarding1Subtitle',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Instant Updates`
+  String get onboarding2Title {
+    return Intl.message(
+      'Instant Updates',
+      name: 'onboarding2Title',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `All inventory changes are synchronized in real time, providing up-to-date stock information and enhancing decision-making processes.`
+  String get onboarding2Subtitle {
+    return Intl.message(
+      'All inventory changes are synchronized in real time, providing up-to-date stock information and enhancing decision-making processes.',
+      name: 'onboarding2Subtitle',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Continue`
+  String get continueButton {
+    return Intl.message('Continue', name: 'continueButton', desc: '', args: []);
+  }
+
+  /// `Skip`
+  String get skipButton {
+    return Intl.message('Skip', name: 'skipButton', desc: '', args: []);
+  }
+
+  /// `Get Started`
+  String get getStartedButton {
+    return Intl.message(
+      'Get Started',
+      name: 'getStartedButton',
+      desc: '',
+      args: [],
+    );
+  }
+}
+
+class AppLocalizationDelegate extends LocalizationsDelegate<S> {
+  const AppLocalizationDelegate();
+
+  List<Locale> get supportedLocales {
+    return const <Locale>[
+      Locale.fromSubtags(languageCode: 'en'),
+      Locale.fromSubtags(languageCode: 'ar'),
+    ];
+  }
+
+  @override
+  bool isSupported(Locale locale) => _isSupported(locale);
+  @override
+  Future<S> load(Locale locale) => S.load(locale);
+  @override
+  bool shouldReload(AppLocalizationDelegate old) => false;
+
+  bool _isSupported(Locale locale) {
+    for (var supportedLocale in supportedLocales) {
+      if (supportedLocale.languageCode == locale.languageCode) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
