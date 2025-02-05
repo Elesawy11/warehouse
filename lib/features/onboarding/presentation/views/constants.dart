6a542f7e22f61602ab978fc4e70092edea48a464
AhmedElesawy <elesawy928@gmail.com>
2025-02-05 14:42:05 +0200

    get onbaording list method

---
+++ lib/features/onboarding/presentation/views/constants.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import '../../../../core/utils/assets.dart';
+import '../../../../generated/l10n.dart';
+
+abstract class Constants {
+  static List<List<String>> getOnbaordingList(BuildContext context) {
+    return [
+      [
+        Assets.imagesOnboarding1,
+        S.of(context).onboarding1Title,
+        S.of(context).onboarding1Subtitle,
+      ],
+      [
+        Assets.imagesOnboarding3,
+        S.of(context).onboarding2Title,
+        S.of(context).onboarding2Subtitle,
+      ],
+      [
+        Assets.imagesOnboarding2,
+        S.of(context).onboarding3Title,
+        S.of(context).onboarding3Subtitle,
+      ],
+    ];
+  }
+}
