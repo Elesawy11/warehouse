ce9fb192fb8761e27b2c269825e141e763ade0de
AhmedElesawy <elesawy928@gmail.com>
2025-06-26 19:18:26 +0300

    change checkout button logic in signup widget

--- lib/features/Auth/presentation/views/widgets/sign_up_view_widget.dart
+++ lib/features/Auth/presentation/views/widgets/sign_up_view_widget.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:warehouse_app/features/Auth/presentation/cubits/sign_up_cubit/sign_up_cubit.dart';
-import 'package:warehouse_app/features/Auth/presentation/cubits/sign_up_variables_cubit/sign_up_variables_cubit.dart';
 import '../../../../../core/helpers/spacer.dart';
 import 'sign_up_check_box_widget.dart';
 import 'sign_up_create_account_button_widget.dart';
@@ -11,21 +10,19 @@ class SignUpViewWidget extends StatelessWidget {
   const SignUpViewWidget({super.key});
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => SignUpVariablesCubit(),
-      child: SingleChildScrollView(
-        child: Form(
-          key: context.read<SignUpCubit>().formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SignUpDataFields(),
-              verticalSpace(20),
-              const SignUpCheckBoxWidget(),
-              verticalSpace(40),
-              const SignUpCreateAccountButtonWidget(),
-            ],
-          ),
+    return SingleChildScrollView(
+      child: Form(
+        key: context.read<SignUpCubit>().formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SignUpDataFields(),
+            verticalSpace(20),
+            const SignUpCheckBoxWidget(),
+            verticalSpace(40),
+            const SignUpCreateAccountButtonWidget(),
+            verticalSpace(20),
+          ],
         ),
       ),
     );
