4ac5bf272476d37fabc644edcc60e00b607a4b46
AhmedElesawy <elesawy928@gmail.com>
2025-06-30 01:21:05 +0300

    update ui

--- lib/features/Auth/presentation/cubits/log_in_variables_cubit/log_in_variables_state.dart
+++ lib/features/Auth/presentation/cubits/log_in_variables_cubit/log_in_variables_state.dart
@@ -1,8 +1,8 @@
-// part of 'log_in_variables_cubit.dart';
+part of 'log_in_variables_cubit.dart';
 
-// @immutable
-// sealed class LogInVariablesState {}
+@immutable
+sealed class LogInVariablesState {}
 
-// final class LogInVariablesInitial extends LogInVariablesState {}
-// final class IsCheckedTrue extends LogInVariablesState {}
-// final class IsCheckedFalse extends LogInVariablesState {}
+final class LogInVariablesInitial extends LogInVariablesState {}
+final class IsCheckedTrue extends LogInVariablesState {}
+final class IsCheckedFalse extends LogInVariablesState {}
